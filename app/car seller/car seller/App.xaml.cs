c187d6c9fdb1818fee4bfd04b88dd40f59d19a50
DelicaLib <danil-nikonov-2017@mail.ru>
2024-01-06 11:46:50 +0300

    mobile app init

---
+++ app/car seller/car seller/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using car_seller.Services;
+using car_seller.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace car_seller
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ app/car seller/car seller/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using car_seller.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace car_seller.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
